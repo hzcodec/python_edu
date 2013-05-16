c13dce6d23b736a1fe9e165b340a42b20b39a3b2
Heinz Samuelsson <heinz.samuelsson@gmail.com>
2013-05-16 22:44:13 +0200

    traffic updated.

--- games/simulate_car.py
+++ games/simulate_car.py
@@ -8,7 +8,8 @@
 time_resolution = 0.1 #seconds
 
 class Car:
-    def __init__(self,name,acc,deacc,acc_time):
+
+    def __init__(self,name,acc,deacc,acc_time,react):
         self.acceleration   = acc       # acceleration m/s^2
         self.deacceleration = deacc     # deacceleration m/s^2
         self.acc_time       = acc_time  # s
@@ -16,32 +17,54 @@ class Car:
         self.s              = 0.0       # distance
         self.name           = name      # name of car     
         self.my_time        = 0.0       # current time
+        self.mode           = "STOPPED" # flag if car is running or not
+        self.reaction_time  = react     # reaction time until car start
+
 
     def drive(self,log="disabled"):
-        # calculate distance and final speed, also keep track of current time
-        self.s       = self.acceleration*time_resolution*time_resolution/2 + self.v0 * time_resolution + self.s
-        self.v0      = self.acceleration*time_resolution + self.v0
+
+        if self.my_time < self.reaction_time:
+            pass # don't do anything, just sit tight
+
+        else:
+            # calculate distance and final speed, also keep track of current time
+            self.s       = self.acceleration*time_resolution*time_resolution/2 + self.v0 * time_resolution + self.s
+            self.v0      = self.acceleration*time_resolution + self.v0
+            self.mode = "RUNNING"
+
+        # increment time
         self.my_time = self.my_time + time_resolution
 
+        # is logging function turned on?
         if log == "log_enabled":
-            print "  Current time:",self.my_time,"s"
+            print "->  Current time:",self.my_time,"s <-"
+            print "      Distance:",self.s,"m"
+            print "      Velocity:",self.v0,"m/s"
+            print "      Mode:    ",self.mode
 
         # return distance and speed
         return self.s,self.v0
+
 
     def stop(self,log="disabled"):
         self.s       = self.deacceleration*time_resolution*time_resolution/2 + self.v0 * time_resolution + self.s
         self.v0      = self.v0 - self.deacceleration*time_resolution
         self.my_time = self.my_time + time_resolution
 
-        if log == "log_enabled":
-            print "  Current time:",self.my_time,"s"
-
         # just to fix very small values
         if self.v0 < 0.01:
             self.v0 = 0.0
+            self.mode = "STOPPED"
+
+        if log == "log_enabled":
+            print "->  Current time:",self.my_time,"s <-"
+            print "      Distance:",self.s,"m"
+            print "      Velocity:",self.v0,"m/s"
+            print "      Mode:    ",self.mode
+
         # return distance and speed
         return self.s,self.v0
+
 
     def car_information(self):
         print 30*"-"
@@ -49,25 +72,26 @@ class Car:
         print "Acceleration:     ",self.acceleration,"m/s^2"
         print "Deacceleration:   ",self.deacceleration,"m/s^2"
         print "Acceleration time:",self.acc_time,"s"
+        print "Mode:             ",self.mode
         print 30*"-"
+
+
+    def get_mode(self,car):
+        return car.mode
 
 
 if __name__ == "__main__":
 
-    car = Car("Volvo",2.0,4.0,10.0)
+    car = Car("Volvo",2.0,4.0,10.0,0.0)
     sim_time = 11
     car.car_information()
 
     print "*** drive initated for car ***"
     for i in range(1,sim_time):
         distance,velocity = car.drive("log_enabled")
-        print "    Distance:",distance,"m"
-        print "    Velocity:",velocity,"m/s"
 
     print "*** stop initated for car ***"
     while velocity > 0.01:
         distance,velocity = car.stop("log_enabled")
-        print "    Distance:",distance,"m"
-        print "    Velocity:",velocity,"m/s"
 
     print "*** car stopped ***"
--- games/traffic.py
+++ games/traffic.py
@@ -8,37 +8,35 @@ import simulate_car as SimCar
 
 class Traffic:
     def __init__(self,sim_time):
-        self.car1 = SimCar.Car("Mazda",1.5,3.0,10.0)
+        self.car1 = SimCar.Car("Mazda",3.5,3.0,10.0,1.5)
         self.distance1 = 0.0
         self.velocity1 = 0.0
 
-        self.car2 = SimCar.Car("Volvo",2.0,4.0,10.0)
+        self.car2 = SimCar.Car("Volvo",2.0,4.0,10.0,0.5)
         self.distance2 = 0.0
         self.velocity2 = 0.0
 
         self.sim_time  = sim_time
 
-    def start(self):
+    def start(self,log="disabled"):
 
-        print "*** drive initated for:",self.car1.name
+        print "*** drive initated for:",self.car1.name," ***"
         for i in range(1,self.sim_time):
-            self.distance1,self.velocity1 = self.car1.drive(0.1)
-        print "  Distance:",self.distance1,"m"
-        print "  Velocity:",self.velocity1,"m/s"
+            self.distance1,self.velocity1 = self.car1.drive(log)
 
-        print "*** drive initated for:",self.car2.name
+        print "*** drive initated for:",self.car2.name," ***"
         for i in range(1,self.sim_time):
-            self.distance2,self.velocity2 = self.car2.drive(0.1)
-        print "  Distance:",self.distance2,"m"
-        print "  Velocity:",self.velocity2,"m/s"
+            self.distance2,self.velocity2 = self.car2.drive(log)
 
 
     def check_distance(self):
         pass
 
+
 if __name__ == "__main__":
    
-    sim_time = 11
+    sim_time = 41
   
     traffic = Traffic(sim_time)
-    traffic.start()
+    print 60*'+'
+    traffic.start("log_enabled")
