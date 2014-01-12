ef9e907d9ca6ff4dd56547a9104e34a077e0f1ad
Heinz <heinz@RaspberryRed.(none)>
2014-01-12 14:46:18 +0000

    unit_rotate3 added. Code cleanup.

--- pygame/unit_rotate3.py
+++ pygame/unit_rotate3.py
@@ -1,25 +1,23 @@
 #   Auther      : Heinz Samuelsson
 #   Date        : 2014-01-12
-#   File        : unit_rotate2.py
+#   File        : unit_rotate3.py
 #   Reference   : -
 #   Description : Draw lines at different angles. N.B only angles between
-#                 0 - 90 degrees can be handled.
+#                 0 - 90 degrees can be handled. Run the script with 3 parameters.
+#                 The parameters are x position, y position and length of vector.
+#                 Ex.
+#                   > python unit_rotate3.py 250 190 125
 #   Python ver  : 2.7.3 (gcc 4.6.3)
 
 import pygame
 from pygame.locals import *
 import math
+import argparse
 
 SIZE    = 640,480
 RED     = [255,0,0]
 GREEN   = [0,255,0]
 BLUE    = [0,0,255]
-
-# start values
-x_pos  = 200  # x start position
-y_pos  = 260  # y start position
-length = 250  # length of vector
-
 
 # calculate new x_pos and y_pos
 def xy_pos(angle,length):
@@ -34,6 +32,15 @@ def draw_line(color,x_pos,y_pos,length,new_x,new_y):
     t = length - round(new_x)
     pygame.draw.line(screen,color,(x_pos,y_pos),(x_pos+length-t,y_pos-round(new_y)))
 
+# handle in parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("par1",type=int)
+parser.add_argument("par2",type=int)
+parser.add_argument("par3",type=int)
+args   = parser.parse_args()
+x_pos  = args.par1  # x position
+y_pos  = args.par2  # y position
+length = args.par3  # length of vector
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
@@ -42,7 +49,7 @@ done = False
 screen.fill((0,0,0))
 
 
-# draw x,y line
+# draw a red x,y line
 new_x,new_y = xy_pos(0,length)
 draw_line(RED,x_pos,y_pos,length,new_x,new_y)
 new_x,new_y = xy_pos(90,length)
