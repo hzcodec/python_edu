bea7aed95faf6c2e36776fa08f4731670d723b21
Heinz <heinz@RaspberryRed.(none)>
2014-01-15 20:25:25 +0000

    box_rotate3 added.

--- pygame/box_rotate3.py
+++ pygame/box_rotate3.py
@@ -1,8 +1,8 @@
 #   Auther      : Heinz Samuelsson
 #   Date        : 2014-01-14
-#   File        : box_rotate2.py
+#   File        : box_rotate3.py
 #   Reference   : www.pygame.org/wiki/RotateCenter?parent=Cookbook
-#   Description : Rotate a box 60 degress while keeping the image center.
+#   Description : Rotate a box from 1 to 359 degress while keeping the image center.
 #   Python ver  : 2.7.3 (gcc 4.6.3)
 
 import pygame, sys
@@ -10,11 +10,13 @@ from pygame.locals import *
 
 RED         = (255,0,0)
 GREEN       = (0,255,0)
+BLACK       = (0,0,0)
 SCREEN_SIZE = (640,380)
 
 box_image   = "box2.png"
 box_pos_x   = SCREEN_SIZE[0]/2
 box_pos_y   = SCREEN_SIZE[1]/2
+done        = False
 
 # rotate and keep center
 def rot_center(image, rect, angle):
@@ -22,6 +24,13 @@ def rot_center(image, rect, angle):
     rot_image = pygame.transform.rotate(image, angle)
     rot_rect = rot_image.get_rect(center=rect.center)
     return rot_image,rot_rect
+
+# draw a cross
+def draw_cross():
+    
+    # draw cross in middle of box with thickness 2
+    pygame.draw.line(screen,GREEN,(220,285),(460,285),2)
+    pygame.draw.line(screen,GREEN,(334,160),(334,375),2)
 
 
 # initialize the library
@@ -35,30 +44,25 @@ screen.blit(box,(box_pos_x,box_pos_y))
 print 55*'='
 
 box_rect = box.get_rect()
-print "box_rec:",box_rect
 
-# rotate 60 degrees
-rot_image,rot_rect = rot_center(box,box_rect,60)
-print "rot_image:",rot_image
-print "rot_rect[0]:",rot_rect[0]
-print "rot_rect[1]:",rot_rect[1]
+# rotate box from 1 to 360 degrees
+for angle in range(1,361):
 
-# new position for box
-screen.blit(rot_image,((box_pos_x+rot_rect[0]),(box_pos_y+rot_rect[1])))
+    # rotate box
+    rot_image,rot_rect = rot_center(box,box_rect,angle)
+    
+    # new position for box
+    screen.blit(rot_image,((box_pos_x+rot_rect[0]),(box_pos_y+rot_rect[1])))
+    
+    draw_cross()
 
-# draw cross in middle of the screen with thickness 2
-pygame.draw.line(screen,RED,(40,190),(600,190),2)
-pygame.draw.line(screen,RED,(320,20),(320,360),2)
-
-# draw cross in middle of box with thickness 2
-pygame.draw.line(screen,GREEN,(320,285),(348,285),2)
-pygame.draw.line(screen,GREEN,(334,190),(334,372),2)
-
-pygame.display.flip() 
+    pygame.display.flip() 
+    screen.fill((BLACK))
 
 
-while 1:
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+while not done:
+    for e in pygame.event.get():
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+            done = True
+            break
+
