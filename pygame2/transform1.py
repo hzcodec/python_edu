962c651e5019f5fe26bb6b17791324f93fe64389
Heinz <heinz@RaspberryRed.(none)>
2014-01-18 13:40:40 +0000

    transform1 updated.

--- pygame2/transform1.py
+++ pygame2/transform1.py
@@ -2,50 +2,77 @@
 #   Date        : 2014-01-16
 #   File        : transform1.py
 #   Reference   : -
-#   Description : 
+#   Description : Rotate a rectangle around different points.
 #   Python ver  : 2.7.3 (gcc 4.6.3)
 
 import pygame
 from pygame.locals import *
 import math
 
-SIZE   = 640,480
+SIZE   = 800,600
 COORDx = 200
 COORDy = 100
-GREEN  = [0,255,0]
 RED    = [255,0,0]
+GREEN  = [0,255,0]
+BLUE   = [0,0,255]
+YELLOW = [255,255,0]
 
 # rotate angle in degrees
-v = 10
+rotate_angle = 0,45,90,120
 
-# define offset
-x_offset = 0
-y_offset = 0
+# define axis for the green cross
+x_axis = 800
+y_axis = 600
 
+# define offset (in the middle)
+x_offset = 300
+y_offset = 250
+
+# define offset (upper left corner)
+#x_offset = 0
+#y_offset = 0
+
+# define rotation point (upper left corner)
+#x_rotation_point = 0
+#y_rotation_point = 0
+
+# define rotation point (upper right corner)
+x_rotation_point = 200 
+y_rotation_point = 0
+
+# define rotation point (middle point)
+#x_rotation_point = 100
+#y_rotation_point = 50
+
+# define rectangle (200x100)
 p1 = 0,0
-p2 = 100,0
-p3 = 100,300
-p4 = 0,300
+p2 = 200,0
+p3 = 200,100
+p4 = 0,100
 
 
-def get_coordinates(pxy,ox,oy):
-   print pxy[0]
-   print pxy[1]
-   print "A",(pxy[0]-ox)*math.cos(v)
-   print "C",(pxy[0]-ox)*math.cos(v) - (pxy[1]-oy)*math.sin(v) + ox
-   xn = (pxy[0]-ox)*math.cos(v)
-   print "nisse",xn
+def get_coordinates(angle,pxy,ox,oy):
 
-   #xn = (pxy[0]-ox)*math.cos(v) - (pxy[1]-oy)*math.sin(v) + ox
-   yn = (pxy[0]-ox)*math.sin(v) + (pxy[1]-oy)*math.cos(v) + oy
+   v = math.pi*angle/180.0
+   xn = ((pxy[0]-ox) * math.cos(v) - (pxy[1]-oy) * math.sin(v)) + ox
+   yn = ((pxy[0]-ox) * math.sin(v) + (pxy[1]-oy) * math.cos(v)) + oy
+
    return xn,yn
 
 
-def draw_line(px1,py1,px2,py2,px3,py3,px4,py4):
-    pygame.draw_line(screen,RED,(px1,py1),(px2,py2))
-    pygame.draw_line(screen,RED,(px1,py1),(px4,py4))
-    pygame.draw_line(screen,RED,(px4,py4),(px3,py3))
-    pygame.draw_line(screen,RED,(px2,py2),(px3,py3))
+def draw_lines(px1,py1,px2,py2,px3,py3,px4,py4,ox,oy):
+    pygame.draw.line(screen,RED,(px1+ox,py1+oy),(px2+ox,py2+oy))
+    pygame.draw.line(screen,RED,(px1+ox,py1+oy),(px4+ox,py4+oy))
+    pygame.draw.line(screen,RED,(px4+ox,py4+oy),(px3+ox,py3+oy))
+    pygame.draw.line(screen,BLUE,(px2+ox,py2+oy),(px3+ox,py3+oy))
+
+
+def draw_axis():
+    pygame.draw.line(screen,GREEN,(0,y_axis/2),(x_axis,y_axis/2))
+    pygame.draw.line(screen,GREEN,(x_axis/2,0),(x_axis/2,y_axis))
+    pygame.draw.line(screen,YELLOW,(0,50),(200,50))
+    pygame.draw.line(screen,YELLOW,(100,0),(100,100))
+
 
 def print_values(a,b):
     print "xn:",a
@@ -59,23 +86,17 @@ screen = pygame.display.set_mode(SIZE)
 done = False
 screen.fill((0,0,0))
 
-pygame.draw.line(screen,GREEN,(70,225),(350,225))
+for i in range(0,4):
+    rotate_angle[i]
+    px1,py1 = get_coordinates(rotate_angle[i],p1,x_rotation_point,y_rotation_point)
+    px2,py2 = get_coordinates(rotate_angle[i],p2,x_rotation_point,y_rotation_point)
+    px3,py3 = get_coordinates(rotate_angle[i],p3,x_rotation_point,y_rotation_point)
+    px4,py4 = get_coordinates(rotate_angle[i],p4,x_rotation_point,y_rotation_point)
+    
+    draw_axis()
+    draw_lines(px1,py1,px2,py2,px3,py3,px4,py4,x_offset,y_offset)
 
-px1,py1 = get_coordinates(p1,x_offset,y_offset)
-print "kalle",px1,py1
-print_values(px1,py1)
-
-px2,py2 = get_coordinates(p2,x_offset,y_offset)
-print "kalle",px1,py1
-print_values(px1,py1)
-
-px3,py3 = get_coordinates(p3,x_offset,y_offset)
-print "kalle",px1,py1
-print_values(px1,py1)
-
-px4,py4 = get_coordinates(p4,x_offset,y_offset)
-print_values(px1,py1)
-pygame.display.flip()
+    pygame.display.flip()
 
 
 while not done:
