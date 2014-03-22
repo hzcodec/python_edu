5342022fc411becf5c36573a1ee8edab6a324ab3
Heinz <heinz@RaspberryRed.(none)>
2014-03-22 14:41:05 +0100

    memory_100 updated.

--- memory_test/memory_100.py
+++ memory_test/memory_100.py
@@ -1,16 +1,24 @@
+# 
+#   Auther      : Heinz Samuelsson
+#   Date        : 2014-03-22
+#   File        : memory_100.py
+#   Reference   : -
+#   Description : Generating numbers between 10-99 and its corresponding image.
+#                 Images belonging to 10-19 is group 0.
+#                 Images belonging to 20-29 is group 1 and so on.
+#   Python ver  : 2.7.3 (gcc 4.6.3)
+
 import pygame
 from pygame.locals import *
 import time
 import random
 
-# screen size
-SCREEN_SIZE  = [800,500]
-# background color
-BLACK = (0,0,0)
-
-START_VALUE = 10
+SCREEN_SIZE  = [800,500] # screen size
+BLACK        = (0,0,0)   # background color
+NO_OF_GROUPS = 5         # number of image groups
 
 
+# group 0
 class Image_10():
     def __init__(self):
         self.picture = []
@@ -26,6 +34,7 @@ class Image_10():
         self.picture.append(pygame.image.load('pic/pic10/table.jpg').convert())
       
 
+# group 1
 class Image_20():
     def __init__(self):
         self.picture = []
@@ -41,6 +50,7 @@ class Image_20():
         self.picture.append(pygame.image.load('pic/pic20/whale.jpg').convert())
 
 
+# group 2
 class Image_30():
     def __init__(self):
         self.picture = []
@@ -56,6 +66,38 @@ class Image_30():
         self.picture.append(pygame.image.load('pic/pic30/squirell.jpg').convert())
 
 
+# group 3
+class Image_40():
+    def __init__(self):
+        self.picture = []
+        self.picture.append(pygame.image.load('pic/pic40/lamp.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/jessica.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/babian.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/ren.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/tomas.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/nut.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/rubin.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/tellus.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/cock.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic40/jennie.jpg').convert())
+
+
+# group 4
+class Image_50():
+    def __init__(self):
+        self.picture = []
+        self.picture.append(pygame.image.load('pic/pic50/susanne.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/banana.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/coffee.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/sabel.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/tarantulla.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/knife.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/birgitta.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/snow.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/socks.jpg').convert())
+        self.picture.append(pygame.image.load('pic/pic50/milk.jpg').convert())
+
+
 class Memory():
 
     def __init__(self):
@@ -67,6 +109,8 @@ class Memory():
         self.image_container_10 = Image_10()  # Image container
         self.image_container_20 = Image_20()  # Image container
         self.image_container_30 = Image_30()  # Image container
+        self.image_container_40 = Image_40()  # Image container
+        self.image_container_50 = Image_50()  # Image container
         self.img_rnd_no         = 0           # Randomly selected image
         self.x                  = 0           # x-pos for image
         self.y                  = 0           # y-pos for image
@@ -81,9 +125,13 @@ class Memory():
             self.surface.blit(pygame.transform.scale(self.image_container_20.picture[img-1],(160,120)),(x,y))
         elif grp == 2:
             self.surface.blit(pygame.transform.scale(self.image_container_30.picture[img-1],(160,120)),(x,y))
+        elif grp == 3:
+            self.surface.blit(pygame.transform.scale(self.image_container_40.picture[img-1],(160,120)),(x,y))
+        elif grp == 4:
+            self.surface.blit(pygame.transform.scale(self.image_container_50.picture[img-1],(160,120)),(x,y))
 
 
-    # generate random number between 1-10
+    # generate random number between lo and hi
     def generate_rnd_number(self,lo,hi):
         return random.randrange(lo,hi+1)
 
@@ -98,13 +146,13 @@ class Memory():
         elif (event.type == pygame.KEYDOWN) and (event.key == pygame.K_ESCAPE):
             self.running = False
 
-        # generate random number for various objects
+        # generate random number for selected image, x-y coordinates and group
         self.img_rnd_no = self.generate_rnd_number(1,10)
         self.x          = self.generate_rnd_number(5,650)
         self.y          = self.generate_rnd_number(5,390)
-        self.group      = self.generate_rnd_number(0,2)
+        self.group      = self.generate_rnd_number(0,NO_OF_GROUPS-1)
 
-        # calculate the offset for each group
+        # calculate the offset depending of group
         offset = self.group*10 + 10
 
         # render the text
