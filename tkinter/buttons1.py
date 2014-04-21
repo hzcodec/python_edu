5768b0ae3ecc7730d755deeded9057fe187773ab
Heinz <heinz@RaspberryRed.(none)>
2014-04-21 21:21:36 +0200

    buttons1 updated.

--- tkinter/buttons1.py
+++ tkinter/buttons1.py
@@ -7,24 +7,31 @@ CANVAS_WIDTH  = 600
 CANVAS_HEIGHT = 200
 
 
+# switch to red bird
 def switch_bird1(img):
     canvas.create_image(50,50,image=img)
+    canvas.create_text(180,110,font="verdena 14 bold",text="192.168.1.79")
     canvas.pack(padx=10,pady=10)
 
+# switch back to original bird
 def switch_bird2(img):
     canvas.create_image(50,50,image=img)
     canvas.pack(padx=10,pady=10)
 
+# add another bird
 def add_bird(img):
     canvas.create_image(150,50,image=img)
     canvas.pack(padx=10,pady=10)
 
+# pop up hello message
 def hello():
     tkMessageBox.showinfo("Hello there!","Hello again")
 
 
 root = Tk()
+root.title("Button Test")
 
+# read images
 img  = PhotoImage(file='./imgs/chicken.gif')
 img2 = PhotoImage(file='./imgs/chicken2.gif')
 
@@ -39,10 +46,11 @@ canvas = Canvas(root,width=CANVAS_WIDTH,height=CANVAS_HEIGHT,
            relief = 'raised')
 
 canvas.create_image(50,50,image=img)
+canvas.create_text(50,110,font="Arial 20 bold",text="Birdie")
 canvas.pack(padx=10,pady=10)
 
-#textBox = Label(frame,text="(x,y): ")
-#textBox.pack()
+textBox = Label(frame,text=" Text on frame ")
+textBox.pack()
 
 # height and width in text lines
 button1 = Button(frame,text='Bird1',height=1, width=4,command = lambda: switch_bird1(img2))
